aa6f57741d9d79fcb2bc45fd473e2b2a634fe273
Rodrigo Pimentel Bertoni <megabertis@gmail.com>
2025-06-24 18:29:41 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,10 +2,9 @@ const admin = require("firebase-admin");
 
 if (!admin.apps.length) {
   const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
 }
 
-module.exports = admin.firestore(); // exporta o Firestore diretamente
+module.exports = admin.firestore();
