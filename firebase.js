1f78c0c18bb4ee72b8e6780e20176211789c6e48
Rodrigo Pimentel Bertoni <megabertis@gmail.com>
2025-06-26 09:18:26 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,14 @@
-const admin = require("firebase-admin");
+import admin from "firebase-admin";
 
 if (!admin.apps.length) {
-  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n')
+    })
   });
 }
 
-module.exports = admin.firestore();
+const db = admin.firestore();
+export default db;
